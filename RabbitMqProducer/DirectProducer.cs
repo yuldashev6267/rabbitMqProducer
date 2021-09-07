394269b6512320ce09942e21efc466fe4b540037
SarvarYuldashev <yuldashev6267@gmail.com>
2021-09-07 12:05:02 +0500

    Rabbit Mq Producer

---
+++ RabbitMqProducer/DirectProducer.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMqProducer
+{
+   public static class DirectProducer
+    {
+        public static void Producer(IModel channel,string name, string lastName)
+        {
+            channel.ExchangeDeclare("demo-direct-exchange",ExchangeType.Direct);
+                var message = new { Name = name, Message = lastName };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                channel.BasicPublish("demo-direct-exchange", "account.init", null, body);
+               
+        }
+    }
+}
---
+++ RabbitMqProducer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory{ Uri = new Uri("amqp://guest:guest@localhost:5672") };
+            using var connection = factory.CreateConnection(); 
+            using var channel = connection.CreateModel();
+            DirectProducer.Producer(channel, "Sarvar" ,"Matkarimov");
+        }
+    }
+}
